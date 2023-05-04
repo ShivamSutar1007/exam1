2dcbd8c44b86fe7624e85ad47a697ca7b0397555
exam <exam@Bcalab4-m08>
2023-05-04 12:17:10 +0530

    first push

---
+++ CaseToolsExam/src/PiggyBank_231.java
@@ -0,0 +1,16 @@
+
+public class PiggyBank_231 {
+	int num_coins;
+	int coin_value;
+	
+	PiggyBank_231()
+	{
+		this.num_coins = num_coins;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
